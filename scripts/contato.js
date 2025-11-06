9077017260ebaab20470c81e680c1f5e920d09d6
Laysa <laysacibele23@gmail.com>
2025-11-06 11:31:28 -0300

    adicionando funcionalidade de anexar imagens ao formulário de denúncias.

--- scripts/contato.js
+++ scripts/contato.js
@@ -3,24 +3,42 @@ document.addEventListener ('DOMContentLoaded', function(){
 
 const form = document.getElementById('contatoForm');
 const isReportCheckBox = document.getElementById('isReport');
-
 const cpfContainer = document.getElementById('cpf-container');
 const cpfInput = document.getElementById('cpf');
+const imagemInput = document.getElementById('imagem');
+
+const nomeArquivo = document.getElementById('nome-arquivo');
+
 
 function toggleCpfVisibility(){
     if (isReportCheckBox.checked){
         cpfContainer.style.display = 'block';
-        cpfInput.setAttribute('requered', 'requered');
-        console.log("Denúncia marcada: CPF visível e obrigatório.")
+        cpfInput.setAttribute('required', 'required');
+        imagemInput.setAttribute('required', 'required');
+        console.log("Denúncia marcada: CPF e imagem visíveis e obrigatórios.")
     }
     else{
         cpfContainer.style.display = 'none';
-        cpfInput.removeAttribute('requered');
-        cpfInput.log ("Denúncia desmarcada: CPF oculto e opcional.")
+        cpfInput.removeAttribute('required');
+        imagemInput.removeAttribute('required')
+        cpfInput.log ("Denúncia desmarcada: CPF e imagens ocultos e opcionais.")
     }
 }
+
+
 
 isReportCheckBox.addEventListener('change', toggleCpfVisibility);
 
     toggleCpfVisibility();
+
+
+});
+
+input.addEventListener('change', function(){
+    if (this.files && this.files.lenght > 0){
+        nomeArquivo.textContent = this.files[0].name;
+    }
+    else{
+        nomeArquivo.textContent = "Nenhuma imagem selecionada";
+    }
 });
